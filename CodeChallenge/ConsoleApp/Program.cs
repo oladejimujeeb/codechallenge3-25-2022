f68f34300cb071729bc255bbdcb25cade6ab5718
Oladeji Mujib <58317485+oladejimujeeb@users.noreply.github.com>
2022-03-25 12:44:09 +0100

    Add files via upload

---
+++ CodeChallenge/ConsoleApp/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool valid = true;
+            Dictionary<string, int> wordDictionary = new Dictionary<string, int>() { };
+            List<string> word = new List<string>() {"a","b", "c", "c", "c"};
+            List<int> occurence = new List<int>();
+            foreach (var item in word)
+            {
+                if (wordDictionary.ContainsKey(item))
+                {
+                    wordDictionary[item] = (int) wordDictionary[item]+1;
+                }
+                else
+                {
+                    wordDictionary.Add(item,1);
+                }
+            }
+
+            foreach (var key in wordDictionary)
+            {
+                occurence.Add(key.Value);
+            }
+
+            for (int i = 0; i < occurence.Count-1; i++)
+            {
+                if (Math.Abs(occurence[i + 1] - occurence[i]) > 1)
+                {
+                    valid = true;
+                   break;
+                }
+                
+            }
+            Console.WriteLine(valid);
+        }
+    }
+}
---
+++ CodeChallenge/ConsoleApp/Question 1.cs
@@ -0,0 +1,13 @@
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleApp
+{
+    public class Question_1
+    {
+        public void Solution2()
+        {
+            
+        }
+    }
+}
